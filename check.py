d32fe3b9f7939c375995cd895378a71b66db137b
Adam Dej <dejko.a@gmail.com>
2015-09-08 21:34:02 +0200

    Shorten names of Task class fields

--- check.py
+++ check.py
@@ -120,11 +120,11 @@ def taskComplete(logger, test_data):
 
     success = True
     for task in test_data["tasks"]:
-        if not task.task_name:
-            logger.logIssue(logging.ERROR, Issue("Úloha nemá meno!", task.task_filename))
+        if not task.name:
+            logger.logIssue(logging.ERROR, Issue("Úloha nemá meno!", task.filename))
             success = False
-        if not task.task_author:
-            logger.logIssue(logging.ERROR, Issue("Úloha nemá autora!", task.task_filename))
+        if not task.author:
+            logger.logIssue(logging.ERROR, Issue("Úloha nemá autora!", task.filename))
             success = False
     return TestResult.OK if success else TestResult.ERROR
 
@@ -138,9 +138,9 @@ def taskProofreaded(logger, test_data):
 
     success = True
     for task in test_data["tasks"]:
-        if not task.task_proofreader:
+        if not task.proofreader:
             logger.logIssue(logging.WARNING, Issue("Úloha nie je sproofreadovaná!",
-                            task.task_filename))
+                            task.filename))
             success = False
     return TestResult.OK if success else TestResult.WARNING
 
@@ -160,11 +160,11 @@ def taskFirstLetter(logger, test_data):
 
     success = True
     for task in test_data["tasks"]:
-        if not task.task_name.startswith(config[task.task_number-1]):
+        if not task.name.startswith(config[task.number-1]):
             logger.logIssue(logging.ERROR,
                             Issue(("Úloha \"{0}\" nezačína správnym písmenom ({1})!"
-                                  .format(task.task_name, config[task.task_number-1])),
-                                  task.task_filename))
+                                  .format(task.name, config[task.number-1])),
+                                  task.filename))
             success = False
     return TestResult.OK if success else TestResult.ERROR
 
@@ -186,13 +186,13 @@ def taskCorrectPoints(logger, test_data):
 
     success = True
     for task in test_data["tasks"]:
-        task_points = (task.task_points["bodypopis"] + task.task_points["bodyprogram"])
-        if task_points != config[task.task_number-1]:
+        task_points = (task.points["bodypopis"] + task.points["bodyprogram"])
+        if task_points != config[task.number-1]:
             logger.logIssue(logging.ERROR,
                             Issue(("Úloha \"{0}\" nemá spávny počet bodov! Má {1}, má mať {2}."
-                                   .format(task.task_name, task_points,
-                                           config[task.task_number-1])),
-                                  task.task_filename))
+                                   .format(task.name, task_points,
+                                           config[task.number-1])),
+                                  task.filename))
             success = False
     return TestResult.OK if success else TestResult.ERROR
 
@@ -207,7 +207,7 @@ def allTasksPresent(logger, test_data):
     tasks_exist = [False]*8
 
     for task in test_data["tasks"]:
-        tasks_exist[task.task_number-1] = True
+        tasks_exist[task.number-1] = True
 
     for task_number, task_exists in enumerate(tasks_exist):
         if not task_exists:
@@ -226,7 +226,7 @@ def taskSamplesEndWithUnixNewline(logger, test_data):
 
     success = True
     for task in test_data["tasks"]:
-        lines = task.task_plaintext.splitlines(True)
+        lines = task.plaintext.splitlines(True)
         checking = False
         for index, line in enumerate(lines):
             if line.startswith('```vstup') or line.startswith('```vystup'):
@@ -237,7 +237,7 @@ def taskSamplesEndWithUnixNewline(logger, test_data):
                 continue
             if checking and line.endswith('\r\n'):
                 logger.logIssue(logging.ERROR, Issue("Riadok má Windowsácky endline!",
-                                                     task.task_filename, index+1))
+                                                     task.filename, index+1))
                 success = False
     return TestResult.OK if success else TestResult.ERROR
 
@@ -251,7 +251,7 @@ def taskSamplesWhitespace(logger, test_data):
 
     success = True
     for task in test_data["tasks"]:
-        lines = task.task_plaintext.splitlines(True)
+        lines = task.plaintext.splitlines(True)
         checking = False
         for index, line in enumerate(lines):
             if line.startswith('```vstup') or line.startswith('```vystup'):
@@ -262,7 +262,7 @@ def taskSamplesWhitespace(logger, test_data):
                 continue
             if checking and re.match("[^ \t\r\f\v]*[ \t\r\f\v]+$", line):
                 logger.logIssue(logging.ERROR, Issue("Riadok končí whitespace-om!",
-                                                     task.task_filename, index+1))
+                                                     task.filename, index+1))
                 success = False
     return TestResult.OK if success else TestResult.ERROR
 
@@ -274,14 +274,14 @@ def taskSamplesWhitespace(logger, test_data):
 
 class Task():
     def __init__(self, task_filename=None, task_text=None):
-        self.task_plaintext = task_text
-        self.task_filename = task_filename
+        self.plaintext = task_text
+        self.filename = task_filename
 
-        self.task_name = None
-        self.task_number = None
-        self.task_points = {}
-        self.task_author = None
-        self.task_proofreader = None
+        self.name = None
+        self.number = None
+        self.points = {}
+        self.author = None
+        self.proofreader = None
 
 
 def parse_task(logger, task_filename):
@@ -289,13 +289,13 @@ def parse_task(logger, task_filename):
     task_file = open(task_filename, 'rb')
     task = Task(task_filename, task_file.read().decode("utf-8"))
 
-    lines = task.task_plaintext.splitlines()
+    lines = task.plaintext.splitlines()
 
     # Vyparsujeme číslo príkladu
-    fname = os.path.basename(task.task_filename)
+    fname = os.path.basename(task.filename)
     try:
         found_task_number = re.search('prikl([0-9]*)', fname).group(1)
-        task.task_number = int(found_task_number)
+        task.number = int(found_task_number)
     except (AttributeError, IndexError, ValueError):
         logger.logIssue(logging.ERROR, Issue("Súbor nemá platné číslo príkladu!", fname))
         return None
@@ -304,7 +304,7 @@ def parse_task(logger, task_filename):
     try:
         # Regex ktoý chce matchnúť meno príkladu (po # a pred {} s bodmi)
         found_task_name = re.search('\.?#([^{}]*)', lines[0]).group(1)
-        task.task_name = found_task_name.strip()
+        task.name = found_task_name.strip()
     except (AttributeError, IndexError):
         logger.logIssue(logging.WARNING, Issue("Nepodarilo sa zistiť meno príkladu!", fname))
 
@@ -312,8 +312,8 @@ def parse_task(logger, task_filename):
     try:
         # Regex ktorý chce matchnúť čísleka s bodmi
         found_points = re.search('{bodypopis=([0-9]*) bodyprogram=([0-9]*)}', lines[0])
-        task.task_points["bodypopis"] = int(found_points.group(1))
-        task.task_points["bodyprogram"] = int(found_points.group(2))
+        task.points["bodypopis"] = int(found_points.group(1))
+        task.points["bodyprogram"] = int(found_points.group(2))
     except (AttributeError, IndexError, ValueError):
         logger.logIssue(logging.WARNING, Issue("Nepodarilo sa zistiť body za príklad!", fname))
 
@@ -321,17 +321,17 @@ def parse_task(logger, task_filename):
         # Vyparsujeme autora
         found_author = re.search('%by (.*)', line)
         if found_author:
-            if task.task_author is not None:
-                logger.warning('Úloha %s má údajne viac autorov!', task.task_filename)
-            task.task_author = found_author.group(1)
+            if task.author is not None:
+                logger.warning('Úloha %s má údajne viac autorov!', task.filename)
+            task.author = found_author.group(1)
 
         # Vyparsujeme proofreadera
         found_proofreader = re.search('%proofread (.*)', line)
         if found_proofreader:
-            if task.task_proofreader is not None:
+            if task.proofreader is not None:
                 logger.logIssue(logging.WARNING, Issue('Úloha má údajne viac proofreaderov!',
                                                        fname, idx+1))
-            task.task_proofreader = found_proofreader.group(1)
+            task.proofreader = found_proofreader.group(1)
 
     return task
 
