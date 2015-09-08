5740e820949a00cba5bfabca7d9d893515bc0c75
Adam Dej <dejko.a@gmail.com>
2015-09-08 19:30:03 +0200

    Restructure task parser, make it use logging style similar to that of tests

--- check.py
+++ check.py
@@ -184,56 +184,6 @@ def taskCorrectPoints(logger, test_data):
 
 
 class Task():
-    def parse(self):
-        logger = logging.getLogger('checker.parser')
-        lines = self.task_plaintext.split('\n')
-
-        # Vyparsujeme číslo príkladu
-        fname = os.path.basename(self.task_filename)
-        try:
-            found_task_number = re.search('prikl([0-9]*)', fname).group(1)
-            self.task_number = int(found_task_number)
-        except (AttributeError, IndexError, ValueError):
-            logger.error('Súbor %s nie je valídne meno príkladu!', self.task_filename)
-
-        # Vyparsujme meno príkladu
-        try:
-            # Regex ktoý chce matchnúť meno príkladu (po # a pred {} s bodmi)
-            found_task_name = re.search('\.?#([^{}]*)', lines[0]).group(1)
-            self.task_name = found_task_name.strip()
-            logger.debug('Vyparsované meno príkladu (%s) z %s', self.task_name, self.task_filename)
-        except (AttributeError, IndexError):
-            logger.warning('Nepodarilo sa vyparsovať meno príkladu z %s', self.task_filename)
-
-        # Vyparsujeme body za príklad
-        try:
-            # Regex ktorý chce matchnúť čísleka s bodmi
-            found_points = re.search('{bodypopis=([0-9]*) bodyprogram=([0-9]*)}', lines[0])
-            self.task_points["bodypopis"] = int(found_points.group(1))
-            self.task_points["bodyprogram"] = int(found_points.group(2))
-            logger.debug('Vyparsované body (%s) z príkladu %s', self.task_points,
-                         self.task_filename)
-        except (AttributeError, IndexError, ValueError):
-            logger.warning('Nepodarilo sa vyprasovať body z príkladu %s', self.task_filename)
-
-        for line in lines:
-            # Vyparsujeme autora
-            found_author = re.search('%by (.*)', line)
-            if found_author:
-                if self.task_author is not None:
-                    logger.warning('Úloha %s má údajne viac autorov!', self.task_filename)
-                self.task_author = found_author.group(1)
-                logger.debug('Nájdený autor (%s) úlohy %s', self.task_author, self.task_filename)
-
-            # Vyparsujeme proofreadera
-            found_proofreader = re.search('%proofread (.*)', line)
-            if found_proofreader:
-                if self.task_proofreader is not None:
-                    logger.warning('Úloha %s má údajne viac proofreaderov!', self.task_filename)
-                self.task_proofreader = found_proofreader.group(1)
-                logger.debug('Nájdený proofreader (%s) úlohy %s', self.task_proofreader,
-                             self.task_filename)
-
     def __init__(self, task_filename=None, task_text=None):
         self.task_plaintext = task_text
         self.task_filename = task_filename
@@ -244,10 +194,56 @@ class Task():
         self.task_author = None
         self.task_proofreader = None
 
-        if self.task_plaintext is not None:
-            self.parse()
 
-        pass
+def parse_task(logger, task_filename):
+    task_file = open(task_filename, 'r')
+    task = Task(task_filename, task_file.read())
+
+    lines = task.task_plaintext.splitlines()
+
+    # Vyparsujeme číslo príkladu
+    fname = os.path.basename(task.task_filename)
+    try:
+        found_task_number = re.search('prikl([0-9]*)', fname).group(1)
+        task.task_number = int(found_task_number)
+    except (AttributeError, IndexError, ValueError):
+        logger.logIssue(logging.ERROR, Issue("Súbor nemá platné číslo príkladu!", fname))
+        return None
+
+    # Vyparsujme meno príkladu
+    try:
+        # Regex ktoý chce matchnúť meno príkladu (po # a pred {} s bodmi)
+        found_task_name = re.search('\.?#([^{}]*)', lines[0]).group(1)
+        task.task_name = found_task_name.strip()
+    except (AttributeError, IndexError):
+        logger.logIssue(logging.WARNING, Issue("Nepodarilo sa zistiť meno príkladu!", fname))
+
+    # Vyparsujeme body za príklad
+    try:
+        # Regex ktorý chce matchnúť čísleka s bodmi
+        found_points = re.search('{bodypopis=([0-9]*) bodyprogram=([0-9]*)}', lines[0])
+        task.task_points["bodypopis"] = int(found_points.group(1))
+        task.task_points["bodyprogram"] = int(found_points.group(2))
+    except (AttributeError, IndexError, ValueError):
+        logger.logIssue(logging.WARNING, Issue("Nepodarilo sa zistiť body za príklad!", fname))
+
+    for idx, line in enumerate(lines):
+        # Vyparsujeme autora
+        found_author = re.search('%by (.*)', line)
+        if found_author:
+            if task.task_author is not None:
+                logger.warning('Úloha %s má údajne viac autorov!', task.task_filename)
+            task.task_author = found_author.group(1)
+
+        # Vyparsujeme proofreadera
+        found_proofreader = re.search('%proofread (.*)', line)
+        if found_proofreader:
+            if task.task_proofreader is not None:
+                logger.logIssue(logging.WARNING, Issue('Úloha má údajne viac proofreaderov!',
+                                                       fname, idx))
+            task.task_proofreader = found_proofreader.group(1)
+
+    return task
 
 
 # -----------------------------------------------------------------------------
@@ -274,7 +270,7 @@ def print_tests():
         print()
 
 
-def parse_tasks(path_to_tasks):
+def parse_tasks(IssueLogger, path_to_tasks):
     VALID_TASK_FILE_NAME = 'prikl*.md'
     tasks = []
     if not os.path.isdir(path_to_tasks):
@@ -282,16 +278,17 @@ def parse_tasks(path_to_tasks):
     for task_filename in glob.iglob(os.path.join(path_to_tasks, VALID_TASK_FILE_NAME)):
         if os.path.isfile(task_filename):
             logger.debug("Čítam zadanie %s", task_filename)
-            task_file = open(task_filename, 'r')
-            tasks.append(Task(task_filename, task_file.read()))
+            task = parse_task(IssueLogger("checker.parser.task"), task_filename)
+            if task is not None:
+                tasks.append(task)
     return tasks
 
 
-def parse_inputs(path_to_inputs):
+def parse_inputs(IssueLogger, path_to_inputs):
     pass
 
 
-def parse_solutions(path_to_solutions):
+def parse_solutions(IssueLogger, path_to_solutions):
     pass
 
 
@@ -329,15 +326,15 @@ def execute(args, tests):
 
     if args.path_to_tasks:
         logger.debug("Spúšťam testy na zadaniach z '%s'", args.path_to_tasks[0])
-        tasks = parse_tasks(args.path_to_tasks[0])
+        tasks = parse_tasks(ConsoleIssueLogger, args.path_to_tasks[0])
 
     if args.path_to_inputs:
         logger.debug("Spúšťam testy na vstupoch z '%s'", args.path_to_inputs[0])
-        inputs = parse_inputs(args.path_to_inputs[0])
+        inputs = parse_inputs(ConsoleIssueLogger, args.path_to_inputs[0])
 
     if args.path_to_solutions:
         logger.debug("Spúšťam testy na vzorákoch z '%s'", args.path_to_solutions[0])
-        solutions = parse_solutions(args.path_to_solutions[0])
+        solutions = parse_solutions(ConsoleIssueLogger, args.path_to_solutions[0])
 
     test_data = {"path_to_tasks": args.path_to_tasks[0] if args.path_to_tasks is not None else None,
                  "path_to_solutions": (args.path_to_solutions[0]
