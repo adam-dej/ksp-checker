24e4016b2f38d3946cf7b0e3d729662a50f0da96
Adam Dej <dejko.a@gmail.com>
2015-09-10 12:11:54 +0200

    Code review fixes

--- check.py
+++ check.py
@@ -25,7 +25,7 @@
 # Dekorátory
 # ----------
 #
-# Dekorátory je treba používať v poradí v akom sú tu popísané. Všetky dekorátory iné ako @test
+# Dekorátory *je treba* používať v poradí v akom sú tu popísané. Všetky dekorátory iné ako @test
 # sú len pre pohodlnosť pri písaní testov a nemusia byť použité.
 #
 # @test: Týmto dekorátorom sa registruje test. Každá funkcia obsahujúca tento dekorátor je
@@ -51,12 +51,12 @@
 # Najskôr beží `main`. Jej úlohou je sparsovať argumenty ktore boli scriptu dané. Na základe týchto
 # argumentov spúšťa ostatné časti scriptu. Obvykle upraví zoznam testov ktoré majú bežať a spustí
 # funkciu `execute`. Táto funkcia zoberie cesty k zadaniam, vstupom a vzorákom a spustí na nich
-# funkcie `parse_{files,inputs}` (parsovanie úloh a vzorákov je tak podobné že to rieši jedna fcia
-# `parse_files`). Tieto funkcie sa preiterujú súbormi so správnym menom, a na každom súbore spustia
-# `parse_{task,solution}`. Tieto funkcie sparsujú už konkrétny súbor. Sú umiestnené v časti skriptu
-# "PARSER" a určené na pravidelné menenie maintainerom testov pri zmene formátu súborov. Funkcie
-# `parse_{task,input,solution}` vrátia objekty sparsovaných vecí funkciám
-# `parse_{tasks,inputs,solutions}`. Tieto vrátia listy sparsovaných objektov funkcii `execute`. Táto
+# funkcie `parse_{markdown,inputs}` (parsovanie úloh a vzorákov je tak podobné že to rieši jedna
+# fcia `parse_markdown`). Tieto funkcie sa preiterujú súbormi so správnym menom, a na každom súbore
+# spustia `{Task,Solution}.parse`. Tieto funkcie sparsujú už konkrétny súbor. Sú umiestnené v časti
+# skriptu "PARSER" a určené na pravidelné menenie maintainerom testov pri zmene formátu súborov.
+# Funkcie `{Task,Solution}.parse` vrátia objekty sparsovaných vecí funkciám
+# `parse_{markdown,inputs}`. Tieto vrátia listy sparsovaných objektov funkcii `execute`. Táto
 # funkcia z týchto dát postaví `test_data` dict, ktorý sa neskôr bude dávať jednotlivým testom.
 # `execute` následne spustí funkciu `execute_tests` so zoznamom testov, dictom `test_data` a classou
 # loggera ktorú maju testy používať. `execute_tests` následne spúšťa jednotlivé testy (pre každý
@@ -75,6 +75,8 @@
 #  - Nakoľko sa meno fcie testu používa v chybovej hláške o zlyhaní a tak, mená majú dávať zmysel
 #    (prosím, žiadne T_PAAMAYIM_NEKUDOTAYIM ;) )
 #  - Unix-like: "No news are good news". Nevypisujte hlúposti ak nie ste verbose.
+#  - Logging je urobený tak ako je aby sa jednoducho dal zmeniť formát výstupu ak by niekto chcel
+#    script použiť ako linter v editore.
 #  - Tento script je zatiaľ iba v zadaniach kde sa môže správať ako chce. Eventuálne ale bude jeden
 #    build step keď sa budú automaticky v CI buildiť zadania, preto je napísaný tak ako je.
 
@@ -446,7 +448,7 @@ def inputsNoTrailingWhitespace(logger, tests):
         inp_file = open(inp_filename, 'r')
         line_number = 1
         for line in inp_file:
-            if re.match("[^ \t\r\f\v]*[ \t\r\f\v]+$", line):
+            if line.rstrip() + '\n' != line:
                 logger.logIssue(logging.WARNING,
                                 Issue("Vstup má na konci riadku whitespaces!", inp_filename,
                                       line_number))
@@ -506,6 +508,63 @@ class Task():
 
         self.bypass = []
 
+    @staticmethod
+    def parse(logger, task_filename):
+        # Binary read mode lebo zachovajme newlines
+        task_file = open(task_filename, 'rb')
+        task = Task(task_filename, task_file.read().decode("utf-8"))
+
+        lines = task.plaintext.splitlines()
+
+        # Vyparsujeme číslo príkladu
+        fname = os.path.basename(task.filename)
+        try:
+            found_task_number = re.search('prikl([0-9]*)', fname).group(1)
+            task.number = int(found_task_number)
+        except (AttributeError, IndexError, ValueError):
+            logger.logIssue(logging.ERROR, Issue("Súbor nemá platné číslo príkladu!", fname))
+            return None
+
+        # Vyparsujme meno príkladu
+        try:
+            # Regex ktoý chce matchnúť meno príkladu (po # a pred {} s bodmi)
+            found_task_name = re.search('\.?#([^{}]*)', lines[0]).group(1)
+            task.name = found_task_name.strip()
+        except (AttributeError, IndexError):
+            logger.logIssue(logging.WARNING, Issue("Nepodarilo sa zistiť meno príkladu!", fname))
+
+        # Vyparsujeme body za príklad
+        try:
+            # Regex ktorý chce matchnúť čísleka s bodmi
+            found_points = re.search('{bodypopis=([0-9]*) bodyprogram=([0-9]*)}', lines[0])
+            task.points["bodypopis"] = int(found_points.group(1))
+            task.points["bodyprogram"] = int(found_points.group(2))
+        except (AttributeError, IndexError, ValueError):
+            logger.logIssue(logging.WARNING, Issue("Nepodarilo sa zistiť body za príklad!", fname))
+
+        for idx, line in enumerate(lines):
+            # Vyparsujeme autora
+            found_author = re.search('%by (.*)', line)
+            if found_author:
+                if task.author is not None:
+                    logger.warning('Úloha %s má údajne viac autorov!', task.filename)
+                task.author = found_author.group(1)
+
+            # Vyparsujeme ktoré testy máme preskočiť
+            found_skiptest = re.search('%skiptest (.*)', line)
+            if found_skiptest:
+                task.bypass.append(found_skiptest.group(1))
+
+            # Vyparsujeme proofreadera
+            found_proofreader = re.search('%proofread (.*)', line)
+            if found_proofreader:
+                if task.proofreader is not None:
+                    logger.logIssue(logging.WARNING, Issue('Úloha má údajne viac proofreaderov!',
+                                                           fname, idx+1))
+                task.proofreader = found_proofreader.group(1)
+
+        return task
+
 
 class Solution():
     def __init__(self, solution_filename=None, solution_text=None):
@@ -519,107 +578,51 @@ class Solution():
 
         self.bypass = []
 
+    @staticmethod
+    def parse(logger, solution_filename):
+        # Binary read mode lebo zachovajme newlines
+        solution_file = open(solution_filename, 'rb')
+        solution = Solution(solution_filename, solution_file.read().decode("utf-8"))
 
-def parse_task(logger, task_filename):
-    # Binary read mode lebo zachovajme newlines
-    task_file = open(task_filename, 'rb')
-    task = Task(task_filename, task_file.read().decode("utf-8"))
+        lines = solution.plaintext.splitlines()
 
-    lines = task.plaintext.splitlines()
+        # Vyparsujeme číslo príkladu
+        fname = os.path.basename(solution.filename)
+        try:
+            found_solution_number = re.search('prikl([0-9]*)', fname).group(1)
+            solution.number = int(found_solution_number)
+        except (AttributeError, IndexError, ValueError):
+            logger.logIssue(logging.ERROR, Issue("Súbor nemá platné číslo príkladu!", fname))
+            return None
 
-    # Vyparsujeme číslo príkladu
-    fname = os.path.basename(task.filename)
-    try:
-        found_task_number = re.search('prikl([0-9]*)', fname).group(1)
-        task.number = int(found_task_number)
-    except (AttributeError, IndexError, ValueError):
-        logger.logIssue(logging.ERROR, Issue("Súbor nemá platné číslo príkladu!", fname))
-        return None
+        # Vyparsujme meno príkladu
+        try:
+            # Regex ktoý chce matchnúť meno príkladu (po # a pred {} s bodmi)
+            found_solution_name = re.search('\.?#([^{}]*)', lines[0]).group(1)
+            solution.name = found_solution_name.strip()
+        except (AttributeError, IndexError):
+            logger.logIssue(logging.WARNING, Issue("Nepodarilo sa zistiť meno príkladu!", fname))
 
-    # Vyparsujme meno príkladu
-    try:
-        # Regex ktoý chce matchnúť meno príkladu (po # a pred {} s bodmi)
-        found_task_name = re.search('\.?#([^{}]*)', lines[0]).group(1)
-        task.name = found_task_name.strip()
-    except (AttributeError, IndexError):
-        logger.logIssue(logging.WARNING, Issue("Nepodarilo sa zistiť meno príkladu!", fname))
+        # Vyparsujeme autora a body za príklad
+        try:
+            # Regex ktorý chce matchnúť autora a čísleka s bodmi
+            found = re.search('{vzorak="([^"]*)" bodypopis=([0-9]*) bodyprogram=([0-9]*)}',
+                              lines[0])
+            solution.author = found.group(1)
+            solution.points["bodypopis"] = int(found.group(2))
+            solution.points["bodyprogram"] = int(found.group(3))
+        except (AttributeError, IndexError, ValueError):
+            logger.logIssue(logging.WARNING, Issue("Nepodarilo sa zistiť autora a body za príklad!",
+                            fname))
 
-    # Vyparsujeme body za príklad
-    try:
-        # Regex ktorý chce matchnúť čísleka s bodmi
-        found_points = re.search('{bodypopis=([0-9]*) bodyprogram=([0-9]*)}', lines[0])
-        task.points["bodypopis"] = int(found_points.group(1))
-        task.points["bodyprogram"] = int(found_points.group(2))
-    except (AttributeError, IndexError, ValueError):
-        logger.logIssue(logging.WARNING, Issue("Nepodarilo sa zistiť body za príklad!", fname))
+        for idx, line in enumerate(lines):
 
-    for idx, line in enumerate(lines):
-        # Vyparsujeme autora
-        found_author = re.search('%by (.*)', line)
-        if found_author:
-            if task.author is not None:
-                logger.warning('Úloha %s má údajne viac autorov!', task.filename)
-            task.author = found_author.group(1)
+            # Vyparsujeme ktoré testy máme preskočiť
+            found_skiptest = re.search('%skiptest (.*)', line)
+            if found_skiptest:
+                solution.bypass.append(found_skiptest.group(1))
 
-        # Vyparsujeme ktoré testy máme preskočiť
-        found_skiptest = re.search('%skiptest (.*)', line)
-        if found_skiptest:
-            task.bypass.append(found_skiptest.group(1))
-
-        # Vyparsujeme proofreadera
-        found_proofreader = re.search('%proofread (.*)', line)
-        if found_proofreader:
-            if task.proofreader is not None:
-                logger.logIssue(logging.WARNING, Issue('Úloha má údajne viac proofreaderov!',
-                                                       fname, idx+1))
-            task.proofreader = found_proofreader.group(1)
-
-    return task
-
-
-def parse_solution(logger, solution_filename):
-    # Binary read mode lebo zachovajme newlines
-    solution_file = open(solution_filename, 'rb')
-    solution = Solution(solution_filename, solution_file.read().decode("utf-8"))
-
-    lines = solution.plaintext.splitlines()
-
-    # Vyparsujeme číslo príkladu
-    fname = os.path.basename(solution.filename)
-    try:
-        found_solution_number = re.search('prikl([0-9]*)', fname).group(1)
-        solution.number = int(found_solution_number)
-    except (AttributeError, IndexError, ValueError):
-        logger.logIssue(logging.ERROR, Issue("Súbor nemá platné číslo príkladu!", fname))
-        return None
-
-    # Vyparsujme meno príkladu
-    try:
-        # Regex ktoý chce matchnúť meno príkladu (po # a pred {} s bodmi)
-        found_solution_name = re.search('\.?#([^{}]*)', lines[0]).group(1)
-        solution.name = found_solution_name.strip()
-    except (AttributeError, IndexError):
-        logger.logIssue(logging.WARNING, Issue("Nepodarilo sa zistiť meno príkladu!", fname))
-
-    # Vyparsujeme autora a body za príklad
-    try:
-        # Regex ktorý chce matchnúť autora a čísleka s bodmi
-        found = re.search('{vzorak="([^"]*)" bodypopis=([0-9]*) bodyprogram=([0-9]*)}', lines[0])
-        solution.author = found.group(1)
-        solution.points["bodypopis"] = int(found.group(2))
-        solution.points["bodyprogram"] = int(found.group(3))
-    except (AttributeError, IndexError, ValueError):
-        logger.logIssue(logging.WARNING, Issue("Nepodarilo sa zistiť autora a body za príklad!",
-                        fname))
-
-    for idx, line in enumerate(lines):
-
-        # Vyparsujeme ktoré testy máme preskočiť
-        found_skiptest = re.search('%skiptest (.*)', line)
-        if found_skiptest:
-            solution.bypass.append(found_skiptest.group(1))
-
-    return solution
+        return solution
 
 
 # -----------------------------------------------------------------------------
@@ -655,9 +658,9 @@ def parse_markdown(IssueLogger, path_to_files, what):
         if os.path.isfile(filename):
             logger.debug("Čítam súbor %s", filename)
             if what == "tasks":
-                thing = parse_task(IssueLogger("checker.parser.task"), filename)
+                thing = Task.parse(IssueLogger("checker.parser.task"), filename)
             elif what == "solutions":
-                thing = parse_solution(IssueLogger("checker.parser.solution"), filename)
+                thing = Solution.parse(IssueLogger("checker.parser.solution"), filename)
 
             if thing is not None:
                 things.append(thing)
